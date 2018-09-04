d49283c120a0a45c05206cc7f964082052da67d4
AHDiniz <AHDiniz@github.com>
2018-09-04 08:40:23 -0300

    Adding file description to main.c

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+/*
+**********************************
+    ED2-2018-2-TC1: TSP
+**********************************
+    Alan Herculano Diniz
+    Rafael Belmock Pedruzzi
+**********************************
+    main.c: program's entry point
+**********************************
+*/
+
 #include <stdio.h>
 
 int main(int argc, char *argv[])
